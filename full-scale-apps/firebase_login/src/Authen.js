872bd8b37cfb826830b61fa55d6b831b8e11bdff
Youssouph Djiba <youssouph.djiba@q2ebanking.com>
2018-12-18 21:45:40 -0600

    complete firease login auth

--- full-scale-apps/firebase_login/src/Authen.js
+++ full-scale-apps/firebase_login/src/Authen.js
@@ -2,28 +2,16 @@ import React, {Component} from 'react';
 var firebase = require('firebase');
 var uuid = require('uuid');
 
-{/*
-  var config = {
-      apiKey: "AIzaSyAMd8NdR4J6Uss2mWpn1kfdE5ss4vselEc",
-      authDomain: "loginauth-c296c.firebaseapp.com",
-      databaseURL: "https://loginauth-c296c.firebaseio.com",
-      projectId: "loginauth-c296c",
-      storageBucket: "loginauth-c296c.appspot.com",
-      messagingSenderId: "895992495574"
-    };
-    firebase.initializeApp(config);
-  */}
-
-
 var config = {
-    apiKey: "AIzaSyDJxYTx6ex7jJnG5b29ztkGvVRCOO8L07o",
-    authDomain: "authen-1812d.firebaseapp.com",
-    databaseURL: "https://authen-1812d.firebaseio.com",
-    projectId: "authen-1812d",
-    storageBucket: "authen-1812d.appspot.com",
-    messagingSenderId: "425823308292"
+    apiKey: "AIzaSyAMd8NdR4J6Uss2mWpn1kfdE5ss4vselEc",
+    authDomain: "loginauth-c296c.firebaseapp.com",
+    databaseURL: "https://loginauth-c296c.firebaseio.com",
+    projectId: "loginauth-c296c",
+    storageBucket: "loginauth-c296c.appspot.com",
+    messagingSenderId: "895992495574"
   };
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
+
 
 
 class Authen extends Component {
@@ -31,18 +19,22 @@ class Authen extends Component {
   login(event){
     const email = this.refs.email.value;
     const password = this.refs.password.value;
-    console.log(email, password);
 
     const auth = firebase.auth();
 
     const promise = auth.signInWithEmailAndPassword(email, password);
 
     // handle login promise
+    promise.then(user =>{
+      var l_out = document.getElementById('logout');
+      var err = 'Welcome Back '+user.user.email;
+      this.setState({err: err});
+      l_out.classList.remove('hide');
+    });
 
     // catching the error message here
     promise.catch(e => {
       var err = e.message;
-      console.log(err);
       this.setState({err: err});
     });
   };
@@ -50,30 +42,35 @@ class Authen extends Component {
   signup(event){
     const email = this.refs.email.value;
     const password = this.refs.password.value;
-    console.log(email, password);
 
-    var auth = firebase.auth(); // maye we should not set it as a constant
+    const auth = firebase.auth();
 
-    var promise = auth.createUserWithEmailAndPassword(email, password); // maye we should not set it as a constant
-
+    const promise = auth.createUserWithEmailAndPassword(email, password);
     // catching the error message here
     promise.then(user => {
-      var err = "Welcome "+user.email;
+      var err = "Welcome "+user.user.email;
       console.log(user);
-      console.log('user uid:'+user.user.uid);
-      firebase.database().ref('users/'+user.user.uid).set({
-        email: user.email
+      firebase.database().ref('users/'+this.state.uid).set({
+        email: user.user.email
       });
-      console.log(user);
       this.setState({err: err});
     });
 
     promise
     .catch(e => {
       var err = e.message;
-      console.log(err);
       this.setState({err: err});
     });
+  }
+
+  logout(event){
+    firebase.auth().signOut();
+    console.log(event);
+    var l_out = document.getElementById('logout');
+    var err = 'Thanks. Please Come Again';
+    this.setState({err: err});
+
+    l_out.classList.add('hide');
   }
 
   constructor(props){
@@ -86,6 +83,7 @@ class Authen extends Component {
 
     this.login = this.login.bind(this);
     this.signup = this.signup.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   render(){
@@ -96,7 +94,7 @@ class Authen extends Component {
         <p>{this.state.err}</p> {/*passing error message here*/}
         <button onClick={this.login}>Log In</button>
         <button onClick={this.signup}>Sign Up</button>
-        <button>Log Out</button>
+        <button id="logout" className="hide" onClick={this.logout}>Log Out</button>
       </div>
     );
   }
